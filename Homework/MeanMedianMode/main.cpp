06ce3cc8105f6205cf173592bac799dfa05cd7d4
bn2595114 <cinexiii@gmail.com>
2016-09-27 01:37:48 -0700

    Signed-off-by: bn2595114 <cinexiii@gmail.com>

--- Homework/MeanMedianMode/main.cpp
+++ Homework/MeanMedianMode/main.cpp
@@ -7,7 +7,7 @@
 /* 
  * File:   main.cpp
  * Author: Bryan Nguyen
- * Purpose: return mean, median, mode
+ * Purpose: return mean, median, mode, and homework problems
  * Created on September 26, 2016, 12:45 PM
  */
 
@@ -22,11 +22,14 @@ float mean(int *, int);
 void sort(int *, int);
 void prntAry(int *, int);
 float med(int *, int);
-
 int arySize(int);
 int *allocate(int);
-
 void ascSort(int *, int);
+int doSomething(int *, int *);
+string *allName(int);
+int *reverse(int [], int);
+int *crtAry(int [], int);
+int *shift(int [], int);
 int main(int argc, char** argv) {
     int num;
     cout << "Choose the number" << endl;
@@ -35,7 +38,10 @@ int main(int argc, char** argv) {
     cout << "3) 9.2 Test Scores #1" << endl;
     cout << "4) 9.3 Drop Lowest Score" << endl;
     cout << "5) 9.4 Test Scores #2" << endl;
-    
+    cout << "6) 9.5 Pointer Rewrite" << endl;
+    cout << "7) 9.10 Reverse Array" << endl;
+    cout << "8) 9.11 Array Expander" << endl;
+    cout << "9) 9.12 Element Shifter" << endl;
     cin >> num;
     
     switch (num)
@@ -72,7 +78,7 @@ int main(int argc, char** argv) {
                 for(int i = 0; i < num; i++)
                     cout << ary[i] << " ";
                 delete []ary; 
-            
+                break;
             }
         case 3:
         {
@@ -88,6 +94,7 @@ int main(int argc, char** argv) {
             cout << endl;
             cout << "Average is: " << mean(ary, scores);
             delete[] ary;
+            break;
         }
         case 4:
         {
@@ -109,6 +116,91 @@ int main(int argc, char** argv) {
             cout << "Average is: " << mean(drop, scores - 1) << endl;
             delete[] ary;
             delete[] drop;
+            break;
+        }
+        case 5: 
+        {
+            int scores, *ary;
+            string *names;
+            cout << "How many test takers??" << endl;
+            cin >> scores;
+            names = allName(scores);
+            ary = allocate(scores);
+            sort(ary, scores);
+            
+            cout << "Test Scores in Ascending Order: " << endl;
+            for (int i = 0; i < scores; i++)
+            {
+                cout << names[i] << ": ";
+                cout << ary[i] << " " << endl;
+            }
+                
+            cout << endl;
+            cout << "Average is: " << mean(ary, scores);
+            delete[] ary;
+            delete[] names;
+            break;
+        }
+        case 6: 
+        {
+            int x, y;
+            cout << "Enter any number (will be multipled by 10): ";
+            cin >> x;
+            cout << "Enter another number (will be multiplied by 10: ";
+            cin >> y;
+            cout << x << " + " << y << " = " << doSomething(&x,&y);
+            break;
+        }
+        case 7:
+        {
+            int *rev;
+            const int SIZE = 5;
+            int array[SIZE] = {1, 2, 3, 4, 5};
+            rev = reverse(array, SIZE);
+            
+            cout << "Array in order: ";
+            for(int i = 0; i < SIZE; i++)
+                cout << array[i] << " ";
+            cout << endl;
+            cout << "Array in reverse: ";
+            for(int i = 0; i < SIZE; i++)
+                cout << rev[i] << " ";
+            
+            delete[] rev; 
+            break;
+        }
+        case 8:
+        {
+            int *doubl;
+            const int SIZE = 5;
+            int ary[SIZE] = {1, 2, 3, 4, 5};
+            cout << "Original Array: ";
+            for(int i = 0; i < SIZE; i++)
+                cout << ary[i] << " ";
+            cout << endl;
+            cout << "Doubled Array Size: ";
+            doubl = crtAry(ary, SIZE);
+            for(int i = 0; i < SIZE*2; i++)
+                cout << doubl[i] << " ";
+            delete[] doubl;
+            break;
+        }
+        case 9:
+        {
+            const int SIZE = 5;
+            int ary[SIZE] = {1, 2, 3, 4, 5};
+            int *shift1;
+            
+            cout << "Original Array: ";
+            for(int i = 0; i < SIZE; i++)
+                cout << ary[i] << " ";
+            cout << endl;
+            cout << "Shifted Array: ";
+            shift1 = shift(ary, SIZE);
+            for(int i = 0; i < SIZE + 1; i ++)
+                cout << shift1[i] << " ";
+            delete[] shift1;
+            break;
         }
     }
     return 0;
@@ -182,10 +274,59 @@ int *allocate(int size)
     int *ary = new int[size];
     for (int i = 0; i < size; i++)
     {
-        cout << "Enter number or test score: " << endl;
+        cout << "Enter number or test score " << i+1 << ": "  << endl;
         cin >> num;
         ary[i] = num;
     }
     return ary;
 }
 
+string *allName(int size)
+{
+    string n;
+    string *ary = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Enter name of test taker " << i+1 << ": " << endl;
+        cin >> n;
+        ary[i] = n;
+    }
+    return ary;
+}
+
+int doSomething(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y * 10;
+    *y = temp * 10;
+    return *x+*y;
+}
+
+int *reverse(int ary[], int size)
+{
+    int *arry = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        arry[i] = ary[size - 1 - i];
+    }
+    return arry;
+}
+
+int *crtAry(int ary[], int size)
+{
+    int *twice = new int[size*2];
+    for(int i = 0; i < size; i++)
+        twice[i] = ary[i];
+    for(int i = size; i < size*2; i++)
+        twice[i] = 0;
+    return twice;
+}
+
+int *shift(int ary[], int size)
+{
+    int *arry = new int[size+1];
+    arry[0] = 0;
+    for(int i = 1; i < size + 1; i++)
+        arry[i] = ary[i - 1];
+    return arry;
+}
